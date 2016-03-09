daac62aad219af11ee81dd53b17b2c5c501c0317
John Cooley <johndavidcooley@gmail.com>
2016-03-08 19:29:01 -0500

    Completed most of the HTML, CSS, and JS.
    Still a little JS debugging to do.

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 // Javascript Entry Point
-import $ from 'jquery'
+import $ from 'jquery';
+
+$('.two').on('click', function() {
+	$('.two').toggleClass('trans-2');
+});
+
+$('.three-h').on('click', function() {
+	$('.three').toggleClass('trans-3');
+});
