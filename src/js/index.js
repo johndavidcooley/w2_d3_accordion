c15edef63e7bbdbddb5e434ef53fd849ee0e4506
John Cooley <johndavidcooley@gmail.com>
2016-03-08 23:26:24 -0500

    Final commit for now.
    Please see the comment in my index.js file.

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,17 @@ $('.two').on('click', function() {
 $('.three-h').on('click', function() {
 	$('.three').toggleClass('trans-3');
 });
+// I do not know why these function
+// are not working. I tried changing
+// classes to ids, altering the Sass,
+// and changing HTML elements to get
+// these to work. When I inspect the
+// element, it shows that the classes
+// .trans-2 and .trans-3 are toggled
+// on and off, but the CSS of the
+// classes seems to be overridden by
+// the classes .two and .three that
+// are established in index.html.
+// I do not know why the positioning
+// of the toggling classes does not
+// override the CSS of the HTML classes.
