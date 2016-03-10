a50e1a504bf1b81df29cb98ad0e69f93c5dc41e1
John Cooley <johndavidcooley@gmail.com>
2016-03-10 12:10:20 -0500

    Updated HTML, CSS, and JS to work.

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,20 @@
 // Javascript Entry Point
 import $ from 'jquery';
 
-$('.two').on('click', function() {
-	$('.two').toggleClass('trans-2');
+$('.one-h').on('click', function() {
+	$('.para-one').toggleClass('trans-one');
+	$('.para-two').removeClass('trans-two');
+	$('.para-three').removeClass('trans-three');
+});
+
+$('.two-h').on('click', function() {
+	$('.para-one').removeClass('trans-one');
+	$('.para-two').toggleClass('trans-two');
+	$('.para-three').removeClass('trans-three');
 });
 
 $('.three-h').on('click', function() {
-	$('.three').toggleClass('trans-3');
+	$('.para-one').removeClass('trans-one');
+	$('.para-two').removeClass('trans-two');
+	$('.para-three').toggleClass('trans-three');
 });
-// I do not know why these function
-// are not working. I tried changing
-// classes to ids, altering the Sass,
-// and changing HTML elements to get
-// these to work. When I inspect the
-// element, it shows that the classes
-// .trans-2 and .trans-3 are toggled
-// on and off, but the CSS of the
-// classes seems to be overridden by
-// the classes .two and .three that
-// are established in index.html.
-// I do not know why the positioning
-// of the toggling classes does not
-// override the CSS of the HTML classes.
